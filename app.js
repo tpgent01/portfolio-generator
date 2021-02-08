35b94f9e2c325f67904d4c8d9fb9f7bdc19debd2
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-08 17:09:53 -0600

    template literals

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-  };
+const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+console.log(generatePage('Jane', 'janehub'));
