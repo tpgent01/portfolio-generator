879a23ebbbfefac26ac6b0a49d42a154669a59bb
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-08 17:18:18 -0600

    destructuring assignment

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
 const generatePage = (userName, githubName) => {
     return `
-      Name: ${userName}
-      GitHub: ${githubName}
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
     `;
   };
-console.log(generatePage('Jane', 'janehub'));
+
+console.log(name, github);
+console.log(generatePage(name, github));
