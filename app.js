15798f83bbf0ab1ffcdfd8cea3720d8a90006fc5
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-10 16:54:43 -0600

    inquirer

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-console.log(profileDataArgs);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-console.log(name, github);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
