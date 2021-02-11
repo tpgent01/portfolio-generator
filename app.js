befad83793f725c15b99867d636b0a3edacd8d75
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-11 17:02:46 -0600

    cleaned up code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const { writeFile, copyFile } = require('./utils/generate-site.js');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site');
 
 const promptUser = () => {
   return inquirer.prompt([
--- utils/generate-site.js
+++ utils/generate-site.js
@@ -19,19 +19,21 @@ const writeFile = fileContent => {
   });
 };
 
+// copying file
 const copyFile = () => {
     return new Promise((resolve, reject) => {
-        fs.copyFile('.dist/index.html', fileContent, err => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve({
-                ok: true,
-                message: 'File copied!'
-            });
+      fs.copyFile('./src/style.css', './dist/style.css', err => {
+        if (err) {
+          reject(err);
+          return;
+        }
+  
+        resolve({
+          ok: true,
+          message: 'Stylesheet created!'
         });
+      });
     });
-};
-
-module.exports = { writeFile, copyFile };
+  };
+  
+  module.exports = { writeFile, copyFile };
