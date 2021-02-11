a8e2fc3d61813a41b8ede65aa8d7cac8d870524c
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-11 16:33:50 -0600

    exported functions

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -19,3 +19,19 @@ const writeFile = fileContent => {
   });
 };
 
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('.dist/index.html', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                ok: true,
+                message: 'File copied!'
+            });
+        });
+    });
+};
+
+module.exports = { writeFile, copyFile };
